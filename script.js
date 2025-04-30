d4f202b4b586686c97bf1f7786a6fa2311e7daff
jerimiahbitancor <jerimiahbitancor27@gmail.com>
2025-05-01 02:25:29 +0800

    done login and register pages

---
+++ script.js
@@ -0,0 +1,9 @@
+function openForm() {
+  document.getElementById("registration").style.display = 'block';
+  document.getElementById("login").style.display = 'none';
+}
+
+function closeForm() {
+  document.getElementById("login").style.display = 'block';
+  document.getElementById("registration").style.display = 'none';
+}
